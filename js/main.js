fe56a57a84f3b11fd25f1b7e576ba1a6a84dc354
Alexandre Testu <atestu@gmail.com>
2013-10-06 19:06:45 -0400

    added playlist 11306085

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function () {
-	var playlists = [4890458, 5673616, 4890458, 5067224, 5346586, 6080329, 6789700, 7432792, 9197291],
+	var playlists = [4890458, 5673616, 4890458, 5067224, 5346586, 6080329, 6789700, 7432792, 9197291, 10059692, 11306085],
 		tracks = [],
 		track = false,
 		sound = false,
@@ -7,14 +7,7 @@
 
 	SC.initialize({ client_id: "4fedbfcc06348cea658479ef1d47ebfb" });
 
-	//+ Jonas Raoni Soares Silva
-	//@ http://jsfromhell.com/array/shuffle [v1.0]
-	function shuffle(o){ //v1.0
-	    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	    return o;
-	};
-
-	playlists = shuffle(playlists);
+	playlists = _.shuffle(playlists);
 
 	playlists.map(function (playlist) {
 		SC.get('/playlists/'+playlist, function(data){
@@ -37,18 +30,12 @@
 		});
 	});
 
-	function hasAlreadyBeenPlayed (track) {
-		return oldTracks.some(function (o) {
-			return o.id == track.id;
-		});
-	}
-
 	function getRandomTrack () {
-		var randomTrack = tracks[Math.floor(Math.random()*tracks.length)];
+		var randomTrack = _.sample(tracks);
 		if (oldTracks.length == tracks.length)
 			oldTracks = [];
-		while (hasAlreadyBeenPlayed (randomTrack) && !randomTrack.streamable)
-			randomTrack = tracks[Math.floor(Math.random()*tracks.length)];
+		while (_.contains(oldTracks, randomTrack) || !randomTrack.streamable)
+			randomTrack = _.sample(tracks);
 		oldTracks.push(randomTrack);
 		return randomTrack;
 	}
@@ -64,7 +51,7 @@
 		}
 		$('#title').text(track.title).attr('href', track.permalink_url);
 		$('.twitter-share-button').remove();
-		$('h2').append(' <a id="twitter-btn" href="https://twitter.com/share" data-text="'+ track.title + ' #MonBeatDTC" data-url="'+ location.href +'"class="twitter-share-button" data-related="monbeatdtc" data-lang="en" data-size="large" data-count="none">Tweet</a>');
+		$('h2').append(' <a id="twitter-btn" href="https://twitter.com/share" data-text="'+ track.title + ' @MonBeatDTC" data-url="'+ location.href +'"class="twitter-share-button" data-related="monbeatdtc" data-size="large" data-count="none">Tweet</a>');
 		twttr.widgets.load();
 
 		SC.stream("/tracks/"+track.id, {
